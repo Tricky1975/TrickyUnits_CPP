efd2d5094c8aecfc6fc94ad76a10440b9279a5fc
Jeroen P. Broks <facebook@tbbs.nl>
2020-08-20 09:21:33 +0200

    Test

---
+++ Headers/TestUnit.h
@@ -0,0 +1,30 @@
+// Lic:
+// ***********************************************************
+// Headers/TestUnit.h
+// This particular file has been released in the public domain
+// and is therefore free of any restriction. You are allowed
+// to credit me as the original author, but this is not
+// required.
+// This file was setup/modified in:
+// 2020
+// If the law of your country does not support the concept
+// of a product being released in the public domain, while
+// the original author is still alive, or if his death was
+// not longer than 70 years ago, you can deem this file
+// "(c) Jeroen Broks - licensed under the CC0 License",
+// with basically comes down to the same lack of
+// restriction the public domain offers. (YAY!)
+// ***********************************************************
+// Version 20.08.19
+// EndLic
+#ifndef INCLUDEDTRICKYUNITTEST
+
+#define INCLUDEDTRICKYUNITTEST
+
+
+namespace TrickyUnits {
+
+	void HelloWorld();
+
+}
+#endif
