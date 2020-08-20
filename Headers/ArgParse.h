2378a924fdbba5971f57a3be7083eb9198382e26
Jeroen P. Broks <facebook@tbbs.nl>
2020-08-20 09:19:38 +0200

    vUpdate

--- Headers/ArgParse.h
+++ Headers/ArgParse.h
@@ -1,7 +1,7 @@
 // Lic:
 // Headers/ArgParse.h
 // Argurment parser
-// version: 20.08.19
+// version: 20.08.20
 // Copyright (C) 2020 Jeroen P. Broks
 // This software is provided 'as-is', without any express or implied
 // warranty.  In no event will the authors be held liable for any damages
