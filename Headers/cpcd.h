03b7b0463fcfab1b140bbc5ef5a40869b4cea91f
Jeroen P. Broks <facebook@tbbs.nl>
2020-09-11 16:20:05 +0200

    Why does Microsoft wanna do everything different than others do?

---
+++ Headers/cpcd.h
@@ -0,0 +1,10 @@
+#ifdef _WIN32
+#include <direct.h>
+// MSDN blocks using getcwd & chdir names
+#define cwd _getcwd
+#define cd _chdir
+#else
+#include "unistd.h"
+#define cwd getcwd
+#define cd chdir
+#endif
