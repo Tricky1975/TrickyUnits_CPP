c1879ce1a8b2caae563bc8fb9221f086303fda19
Jeroen Broks <facebook@tbbs.nl>
2020-11-15 22:30:02 +0100

    vUpdate

--- Source/FileList.cpp
+++ Source/FileList.cpp
@@ -1,7 +1,7 @@
 // Lic:
 // Source/FileList.cpp
 // Tricky's Units - File List
-// version: 20.11.14
+// version: 20.11.15
 // Copyright (C) 2020 Jeroen P. Broks
 // This software is provided 'as-is', without any express or implied
 // warranty.  In no event will the authors be held liable for any damages
