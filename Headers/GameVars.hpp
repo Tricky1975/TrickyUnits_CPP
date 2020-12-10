f0d046d1a44c379daf672b91a5e6b1fa117691df
Jeroen Broks <facebook@tbbs.nl>
2020-12-10 15:29:50 +0100

    Functions I forgot to put in the header... silly me!

--- Headers/GameVars.hpp
+++ Headers/GameVars.hpp
@@ -11,8 +11,10 @@ namespace TrickyUnits {
 	std::map<std::string, std::string>* ReffedVar();
 	std::string VarCall(std::string K, bool Debug = false);
 	std::string VarStr(std::string Str);
+	void VarDef(std::string, std::string value);
 	void VarClearAll();
 	void VarClear(std::string K);
 	size_t VarCount();
 	std::string VarKey(size_t i);
+	std::vector<std::string> Vars();
 }
