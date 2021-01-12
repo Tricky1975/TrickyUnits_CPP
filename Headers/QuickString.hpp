094d8e47e004baf70bfa20cf80846b1cd48e6ac8
Jeroen Broks <facebook@tbbs.nl>
2021-01-13 00:47:59 +0100

    Lic

--- Headers/QuickString.hpp
+++ Headers/QuickString.hpp
@@ -1,8 +1,8 @@
 // Lic:
-// Headers/Quickstd::string.h
-// Quick std::string
-// version: 20.08.20
-// Copyright (C) 2020 Jeroen P. Broks
+// Headers/QuickString.hpp
+// Quick String (header)
+// version: 21.01.13
+// Copyright (C) 2021 Jeroen P. Broks
 // This software is provided 'as-is', without any express or implied
 // warranty.  In no event will the authors be held liable for any damages
 // arising from the use of this software.
