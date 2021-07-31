36c120c8fd6bb262705ef3b49497fe4a24ce0148
Jeroen Broks <facebook@tbbs.nl>
2021-07-31 07:20:16 +0200

    TrickyMath
    (Sometimes C++ is acting strange)

---
+++ Headers/TrickyMath.hpp
@@ -0,0 +1,6 @@
+#pragma once
+namespace TrickyUnits {
+
+	int QMIN(int i, int j);
+	float QMIN(float i, float j);
+}
---
+++ Source/TrickyMath.cpp
@@ -0,0 +1,6 @@
+#include "..\Headers\TrickyMath.hpp"
+namespace TrickyUnits {
+	int QMIN(int i, int j) { if (i < j) return i; return j; }
+	float QMIN(float i, float j) { if (i < j) return i; return j; }
+
+}
