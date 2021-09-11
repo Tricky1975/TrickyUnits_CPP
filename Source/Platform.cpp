bf9ef89a8e7bee9b83ceacb0bd9252c972aaf4aa
Jeroen Broks <facebook@tbbs.nl>
2021-09-11 21:39:03 +0200

    vUpdate

--- Source/Platform.cpp
+++ Source/Platform.cpp
@@ -1,7 +1,7 @@
 // Lic:
 // Source/Platform.cpp
 // Platform
-// version: 21.01.13
+// version: 21.09.11
 // Copyright (C) 2020, 2021 Jeroen P. Broks
 // This software is provided 'as-is', without any express or implied
 // warranty.  In no event will the authors be held liable for any damages
