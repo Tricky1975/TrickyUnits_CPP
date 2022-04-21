bd866a8941b6f17c35ada911c3273345770fa431
Jeroen Broks <facebook@tbbs.nl>
2022-04-21 11:13:34 +0200

    Create QuickTypes.hpp

---
+++ Headers/QuickTypes.hpp
@@ -0,0 +1,19 @@
+#include <string>
+
+namespace TrickyUnits {
+	
+		
+	typedef unsigned char byte;
+	
+	typedef short int int16;
+	typedef unsigned short int uint16;
+	
+	typedef int int32;
+	typedef unsigned int uint32;
+	
+	typedef long long int int64;
+	typedef unsigned long long int uing64;
+	
+	typedef std::string str
+	
+}
