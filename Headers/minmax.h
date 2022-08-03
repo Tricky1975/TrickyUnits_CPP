4b9f3f86580c925b25317883cb69e2e636bbddde
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2022-08-03 14:01:03 +0200

    Create minmax.h

---
+++ Headers/minmax.h
@@ -0,0 +1,12 @@
+// These were merely set up to use whenever I have to use these in C.
+
+
+#define max(a,b) \
+   ({ __typeof__ (a) _a = (a); \
+       __typeof__ (b) _b = (b); \
+     _a > _b ? _a : _b; })
+
+#define min(a,b) \
+   ({ __typeof__ (a) _a = (a); \
+       __typeof__ (b) _b = (b); \
+     _a < _b ? _a : _b; })
