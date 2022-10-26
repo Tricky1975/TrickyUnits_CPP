eec05746790101ec262068d0d5e217365e5813a3
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2022-10-26 23:06:37 +0200

    Update QCol.cpp

--- Source/QCol.cpp
+++ Source/QCol.cpp
@@ -1,3 +1,22 @@
+// Lic:
+// Source/QCol.cpp
+// Quick Color
+// version: 22.10.26
+// Copyright (C) 2022 Jeroen P. Broks
+// This software is provided 'as-is', without any express or implied
+// warranty.  In no event will the authors be held liable for any damages
+// arising from the use of this software.
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it
+// freely, subject to the following restrictions:
+// 1. The origin of this software must not be misrepresented; you must not
+// claim that you wrote the original software. If you use this software
+// in a product, an acknowledgment in the product documentation would be
+// appreciated but is not required.
+// 2. Altered source versions must be plainly marked as such, and must not be
+// misrepresented as being the original software.
+// 3. This notice may not be removed or altered from any source distribution.
+// EndLic
 #include <iostream>
 #include "../Headers/Platform.hpp"
 #include "../Headers/QCol.hpp"
@@ -109,6 +128,8 @@ namespace TrickyUnits{
 	void TmpPlateQCol::Pink(string a) { Write(qColor::Pink, a); }
 	void TmpPlateQCol::Dark(string a) { Write(qColor::DarkGrey, a); }
 	void TmpPlateQCol::Yellow(string a) { Write(qColor::Yellow, a); }
+	void TmpPlateQCol::Cyan(string a) { Write(qColor::Cyan, a); }
+	void TmpPlateQCol::Magenta(string a) { Write(qColor::Magenta, a); }
 
 	TmpPlateQCol::TmpPlateQCol(QColColor c, QColReset r,string n) {
 		Color = c;
