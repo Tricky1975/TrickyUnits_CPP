2648a87b141a195d1cab61d71ec83dcc840595ca
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2022-10-26 23:02:15 +0200

    License block

--- Headers/minmax.h
+++ Headers/minmax.h
@@ -1,12 +1,36 @@
+// Lic:
+// Headers/minmax.h
+// minmax
+// version: 22.10.26
+// Copyright (C) 2022 Jeroen P. Broks
+// This software is provided 'as-is', without any express or implied
+// warranty.  In no event will the authors be held liable for any damages
+// arising from the use of this software.
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it
+// freely, subject to the following restrictions:
+// 1. The origin of this software must not be misrepresented; you must not
+// claim that you wrote the original software. If you use this software
+// in a product, an acknowledgment in the product documentation would be
+// appreciated but is not required.
+// 2. Altered source versions must be plainly marked as such, and must not be
+// misrepresented as being the original software.
+// 3. This notice may not be removed or altered from any source distribution.
+// EndLic
 // These were merely set up to use whenever I have to use these in C.
 
-
-#define max(a,b) \
+/*
+#define tr_max(a,b) \
    ({ __typeof__ (a) _a = (a); \
-       __typeof__ (b) _b = (b); \
-     _a > _b ? _a : _b; })
+	   __typeof__ (b) _b = (b); \
+	 _a > _b ? _a : _b; })
 
-#define min(a,b) \
+#define tr_min(a,b) \
    ({ __typeof__ (a) _a = (a); \
-       __typeof__ (b) _b = (b); \
-     _a < _b ? _a : _b; })
+	   __typeof__ (b) _b = (b); \
+	 _a < _b ? _a : _b; })
+*/
+
+
+void TeddySave(Teddy TeddyMap, std::string filename, std::string dir, std::string Storage);
+void TeddySave(Teddy TeddyMap, jcr6::JT_Create* resource, std::string dir, std::string Storage);
